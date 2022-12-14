07c2d732d83a764c9e7ade239d35654e0f6c5340
Thomas Clark <thomas.clark@bigtincan.com>
2022-12-14 14:08:56 +1100

    format on pre-commit, auto setup git-hooks on install

--- eslint-config-paths.js
+++ eslint-config-paths.js
@@ -3,11 +3,11 @@
 /**
  * Here we define the special rules for importing from package, lib
  * and feature folders.
- * 
+ *
  * All imports of content from a lib or a package folder should be
  * done from `$folder-name`. Imports of content within a feature
  * folder should be done with `@/features/feature-name`.
- * 
+ *
  * The exception to these rules are when a file in a package,lib,
  * or feature folder imports from another file in the same folder.
  * In that case, they must use the full path.
@@ -15,52 +15,73 @@
 
 const fs = require("fs");
 
-const featureFolders = fs.readdirSync("src/features")
-const packageFolders = fs.readdirSync("src/packages")
+const featureFolders = fs.readdirSync("src/features");
+const packageFolders = fs.readdirSync("src/packages");
 const libFiles = fs.readdirSync("src/lib");
 
-const libs = libFiles.map(file => file.replace(".ts", ""))
+const libs = libFiles.map((file) => file.replace(".ts", ""));
 
-const universalPathRestrictions = [...packageFolders.map(folder => ({
-  name: `@/packages/${folder}`,
-  message: `Use the shorthand "$${folder}" instead`
-})), ...libs.map(lib => ({
-  name: `@/lib/${lib}`,
-  message: `Use the shorthand "$${lib}" instead`
-}))]
+const universalPathRestrictions = [
+	...packageFolders.map((folder) => ({
+		name: `@/packages/${folder}`,
+		message: `Use the shorthand "$${folder}" instead`,
+	})),
+	...libs.map((lib) => ({
+		name: `@/lib/${lib}`,
+		message: `Use the shorthand "$${lib}" instead`,
+	})),
+];
 
 /** @type {import('eslint').Linter.BaseConfig} */
 module.exports = {
-  plugins: ["import"],
-  overrides: [
-    ...featureFolders.map((folder) => ({
-      files: [`src/features/${folder}/**/*.{ts,tsx}`],
-      rules: {
-        'no-restricted-imports': ['error', { paths: [...universalPathRestrictions,{
-          name: `@/features/${folder}`,
-          message: 'A feature cannot import from its own index file. Write out the full file path instead.'
-        }], }],
-      }
-    })),
-    ...packageFolders.map((folder) => ({
-      files: [`src/packages/${folder}/**/*.{ts,tsx}`],
-      rules: {
-        'no-restricted-imports': ['error', { paths: [...universalPathRestrictions,{
-          name: `$${folder}`,
-          message: 'A feature cannot import from its own index file. Write out the full file path instead.'
-        }],  }],
-      }
-    }))
-  ],
-  // rules: {
-  //   "no-restricted-imports": ['error', {
-  //     paths: [...packageFolders.map(folder => ({
-  //       name: `@/packages/${folder}`,
-  //       message: `Use the shorthand "$${folder}" instead`
-  //     })), ...libs.map(lib => ({
-  //       name: `@/lib/${lib}`,
-  //       message: `Use the shorthand "$${lib}" instead`
-  //     }))]
-  //   }]
-  // }
-}
+	plugins: ["import"],
+	overrides: [
+		...featureFolders.map((folder) => ({
+			files: [`src/features/${folder}/**/*.{ts,tsx}`],
+			rules: {
+				"no-restricted-imports": [
+					"error",
+					{
+						paths: [
+							...universalPathRestrictions,
+							{
+								name: `@/features/${folder}`,
+								message:
+									"A feature cannot import from its own index file. Write out the full file path instead.",
+							},
+						],
+					},
+				],
+			},
+		})),
+		...packageFolders.map((folder) => ({
+			files: [`src/packages/${folder}/**/*.{ts,tsx}`],
+			rules: {
+				"no-restricted-imports": [
+					"error",
+					{
+						paths: [
+							...universalPathRestrictions,
+							{
+								name: `$${folder}`,
+								message:
+									"A feature cannot import from its own index file. Write out the full file path instead.",
+							},
+						],
+					},
+				],
+			},
+		})),
+	],
+	// rules: {
+	//   "no-restricted-imports": ['error', {
+	//     paths: [...packageFolders.map(folder => ({
+	//       name: `@/packages/${folder}`,
+	//       message: `Use the shorthand "$${folder}" instead`
+	//     })), ...libs.map(lib => ({
+	//       name: `@/lib/${lib}`,
+	//       message: `Use the shorthand "$${lib}" instead`
+	//     }))]
+	//   }]
+	// }
+};
